686f5115dbc7a92d617bb54b9057770d1e49f9da
JDPlusPlus <104108183+JDPlusPlus@users.noreply.github.com>
2024-11-09 12:19:02 -0800

    AHHHHHHHHHHHHHHHH
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- scripts/entities/GameCamera.gd
+++ scripts/entities/GameCamera.gd
@@ -1,27 +1,27 @@
 extends Camera2D
 
-var cameraSpeed : float
-var zoomSpeed : float
-var mouseDown : bool
+var zoomTarget: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	zoomTarget = zoom
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func _unhandled_input(event: InputEvent) -> void:
-	if (event is InputEventMouseButton):
-		var button : InputEventMouseButton = event as InputEventMouseButton
-		mouseDown = button.button_index == MOUSE_BUTTON_MIDDLE && button.pressed
-		
-		
-		zoom.x = clamp(zoom.x - zoomSpeed * (button.button_index == 1 if MOUSE_BUTTON_WHEEL_UP else 0 as float), 0.001,100 )
-		zoom.y = clamp(zoom.y - zoomSpeed * button.button_index == 1 if MOUSE_BUTTON_WHEEL_UP else 0, 0.001,100 )
-		zoom.x = clamp(zoom.x + zoomSpeed * button.button_index == 1 if MOUSE_BUTTON_WHEEL_DOWN else 0, 0.001,100 )
-		zoom.y = clamp(zoom.y + zoomSpeed * button.button_index == 1 if MOUSE_BUTTON_WHEEL_DOWN else 0, 0.001,100 )
-	if (event is InputEventMouseMotion and mouseDown):
-		var motion : InputEventMouseMotion = event as InputEventMouseMotion
-		position += Vector2(motion.relative.x, motion.relative.y) * cameraSpeed
+	var moveVector := Vector2.ZERO
+	if Input.is_action_just_pressed("zoom_in"):
+		zoomTarget *= 1.1
+	if Input.is_action_just_pressed("zoom_out"):
+		zoomTarget *= 0.9
+	
+	if Input.is_action_pressed("pan_right"):
+		moveVector.x += 1500
+	if Input.is_action_pressed("pan_left"):
+		moveVector.x -= 1500
+	if Input.is_action_pressed("pan_up"):
+		moveVector.y -= 1500
+	if Input.is_action_pressed("pan_down"):
+		moveVector.y += 1500
+	offset += moveVector.normalized() * delta * 1500
+	zoom = zoom.slerp(zoomTarget,1)
+	
