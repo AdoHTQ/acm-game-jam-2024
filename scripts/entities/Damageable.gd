47e438043625fca99a4113d30e43b7031d4152c4
AdoHTQ <adamandperry@gmail.com>
2024-11-10 02:41:20 -0800

    Add xp gain on kills

--- scripts/entities/Damageable.gd
+++ scripts/entities/Damageable.gd
@@ -7,5 +7,7 @@ func damage(damage: int) -> void:
 	health -= damage
 	#health = health if health >= 0 else 0
 	
-	if animator == null: return
-	if health <= 0: animator.play("death")
+	if health <= 0: 
+		if get_parent() is Unit: (get_node("Factory/Hero") as Hero).currentExperience += 10
+		if animator == null: return
+		animator.play("death")
