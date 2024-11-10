91f79449c57c527eb8eac0643b43beefaf997ccb
AdoHTQ <adamandperry@gmail.com>
2024-11-10 09:02:47 -0800

    Space laser display fixed

--- scenes/ui/RadioTowerTimer.gd
+++ scenes/ui/RadioTowerTimer.gd
@@ -3,5 +3,5 @@ extends Label
 func _physics_process(delta: float) -> void:
 	var tower: RadTower = get_node_or_null("/root/Factory/RadTower")
 	if tower == null: return
-	
+	if tower.timer.is_stopped(): return
 	text = "Space Laser In:\n%2.2f" % tower.timer.time_left
--- scripts/entities/RadTower.gd
+++ scripts/entities/RadTower.gd
@@ -5,13 +5,9 @@ class_name RadTower
 
 @export var timer: Timer
 
-func _ready() -> void:
-	timer.start()
-	timer.paused = true
-
 func _physics_process(delta: float) -> void:
 	if enabled and timer.is_stopped():
-		timer.paused = false
+		timer.start()
 
 func win():
 	get_tree().change_scene_to_file("res://scenes/ui/Win.tscn")
