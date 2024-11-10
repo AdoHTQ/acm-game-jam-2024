a58b25b6503796f9c870f8ad04a9349838460d1f
AdoHTQ <adamandperry@gmail.com>
2024-11-10 01:20:54 -0800

    Add gear thrower

--- scripts/entities/Projectile.gd
+++ scripts/entities/Projectile.gd
@@ -2,6 +2,7 @@ class_name Projectile extends Area2D
 
 @export var direction: Vector2
 @export var damage: int
+@export var speed: float
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
@@ -12,9 +13,8 @@ func _init() -> void:
 	damage = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.x += direction.x * delta
-	position.y += direction.y * delta
+func _physics_process(delta: float) -> void:
+	position += direction * speed
 	
 func _on_area_entered(area: Area2D) -> void:
 	if area is Damageable:
