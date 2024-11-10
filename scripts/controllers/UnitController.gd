8e3918e722235354a84702d928bf5b2b4933ead7
Caden Feller <caden.feller@gmail.com>
2024-11-09 21:45:00 -0800

    fixed unit selection

--- scripts/controllers/UnitController.gd
+++ scripts/controllers/UnitController.gd
@@ -16,6 +16,7 @@ var selectedUnits: Array[Node2D]
 
 func _ready() -> void:
 	inverseOffset = Vector2(cam.offset.x, cam.offset.y)
+	body_shape_entered.connect(_body_shape_entered)
 
 func _init() -> void:
 	topLeftCoords = Vector2(0, 0)
@@ -31,6 +32,9 @@ func _process(delta: float) -> void:
 	else:
 		rectToDraw = Rect2(-999999, -999999, 0, 0)
 
+func _body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int):
+	selectedUnits.append(body)
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		# this handles selecting a new group of units
@@ -39,17 +43,17 @@ func _input(event: InputEvent) -> void:
 				topLeftCoords = event.position
 				isSelecting = true
 			else:
-				isSelecting = false
+				selectedUnits.clear()
 				bottomRightCoords = event.position
 				position = ((topLeftCoords + bottomRightCoords) / 2) + cam.offset - inverseOffset
-				(collisionShape.shape as RectangleShape2D).extents = (bottomRightCoords - topLeftCoords) / 2
-			
-				# the casting here might not work as expected, if we have issues check this ASAP
-				selectedUnits = get_overlapping_bodies()
+				(collisionShape.shape as RectangleShape2D).extents = (bottomRightCoords - topLeftCoords if bottomRightCoords > topLeftCoords else topLeftCoords - bottomRightCoords) / 2
+
+				isSelecting = false
 
 		# this handles rallying units to the position of the mouse
-		if event.button_index == MOUSE_BUTTON_RIGHT:
+		elif event.button_index == MOUSE_BUTTON_RIGHT:
 			# only check for the press so we don't get double commands
 			if event.pressed:
+				print(selectedUnits)
 				for unit in selectedUnits:
 					unit.destination = event.position + cam.offset - inverseOffset
