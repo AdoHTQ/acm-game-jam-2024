2fcb2bad795d9ce687080c7a262db2f3cd3bad2f
AdoHTQ <adamandperry@gmail.com>
2024-11-09 22:04:47 -0800

    Make hero sprites work

--- scripts/utility/DirectionalSprite.gd
+++ scripts/utility/DirectionalSprite.gd
@@ -1,8 +1,18 @@
 class_name DirectionalSprite extends AnimatedSprite2D
 
+@export var mirrorMode: bool = false
+
 func playAnimation(name: String) -> void:
 	if (animation == name): return
 	playAnimation(name)
 
+func _ready() -> void:
+	play()
+
 func _process(delta: float) -> void:
-	pass;
+	var parent: CharacterBody2D = get_parent()
+	var velocity: Vector2 = parent.velocity
+	
+	if mirrorMode:
+		animation = "walk" if velocity.length() > 0 else "idle"
+		flip_h = velocity.x < 0
