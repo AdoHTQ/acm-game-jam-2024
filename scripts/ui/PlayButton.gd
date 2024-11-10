1182bf39d9666339eed7bfcb24d5116879302a26
Ryan <74388377+ryancoulter317@users.noreply.github.com>
2024-11-09 19:00:52 -0800

    Revert "Merge branch 'main' of https://github.com/AdoHTQ/unr-game-jam-2024"
    This reverts commit 7a79ffb1b00b1725d222fca8838d0e0ee667d34f, reversing
    changes made to d09a5768bfe0f9b90be19661632cebcfe99e3007.

--- scripts/ui/PlayButton.gd
+++ scripts/ui/PlayButton.gd
@@ -1,10 +1,6 @@
-extends Control
+extends Button
 
-func _on_play_button_pressed() -> void:
+
+
+func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/Factory.tscn")
-
-func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
